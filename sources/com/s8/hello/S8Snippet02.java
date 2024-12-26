6db8e5257bf8b1f1ba424e394c9c79d39d4f18e4
Pierre Convert <pierreconvert@macbook-pro-6.home>
2024-12-26 16:59:50 +0100

    Initial commit

---
+++ sources/com/s8/hello/S8Snippet02.java
@@ -0,0 +1,13 @@
+package com.s8.hello;
+
+public class S8Snippet02 {
+
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Just boot here!");
+	}
+}
